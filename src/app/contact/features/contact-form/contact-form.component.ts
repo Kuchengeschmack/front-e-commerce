f6510afa3f6e17ba45e896659d581ca28ce50d4e
Dorian Réale--Caron <drealecaron@protonmail.com>
2024-10-15 16:57:34 +0200

    feat: création de la page de contact

---
+++ src/app/contact/features/contact-form/contact-form.component.ts
@@ -0,0 +1,40 @@
+import {
+  Component,
+  computed,
+  EventEmitter,
+  input,
+  Output,
+  ViewEncapsulation,
+} from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { Product } from "app/products/data-access/product.model";
+import { SelectItem } from "primeng/api";
+import { ButtonModule } from "primeng/button";
+import { DropdownModule } from "primeng/dropdown";
+import { InputNumberModule } from "primeng/inputnumber";
+import { InputTextModule } from "primeng/inputtext";
+import { InputTextareaModule } from 'primeng/inputtextarea';
+
+@Component({
+  selector: "app-contact-form",
+  templateUrl: './contact-form.component.html',
+  styleUrls: ["./contact-form.component.scss"],
+  standalone: true,
+  imports: [
+    FormsModule,
+    ButtonModule,
+    InputTextModule,
+    InputNumberModule,
+    InputTextareaModule,
+    DropdownModule,
+  ],
+  encapsulation: ViewEncapsulation.None
+})
+export class ContactFormComponent {
+  public email: string = "";
+  public message :string = "";
+
+  onSave() {
+    console.log(this.email, this.message)
+  }
+}
