6613e5e58bcc8ca5bb630fa7bad636a60874e7d6
Dorian Réale--Caron <drealecaron@protonmail.com>
2024-10-16 13:58:10 +0200

    feat: création du formulaire de contact

--- src/app/contact/features/contact-form/contact-form.component.ts
+++ src/app/contact/features/contact-form/contact-form.component.ts
@@ -8,16 +8,17 @@ import {
 } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { Product } from "app/products/data-access/product.model";
-import { SelectItem } from "primeng/api";
+import { Message, SelectItem } from "primeng/api";
 import { ButtonModule } from "primeng/button";
 import { DropdownModule } from "primeng/dropdown";
 import { InputNumberModule } from "primeng/inputnumber";
 import { InputTextModule } from "primeng/inputtext";
-import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputTextareaModule } from "primeng/inputtextarea";
+import { MessagesModule } from "primeng/messages";
 
 @Component({
   selector: "app-contact-form",
-  templateUrl: './contact-form.component.html',
+  templateUrl: "./contact-form.component.html",
   styleUrls: ["./contact-form.component.scss"],
   standalone: true,
   imports: [
@@ -27,14 +28,21 @@ import { InputTextareaModule } from 'primeng/inputtextarea';
     InputNumberModule,
     InputTextareaModule,
     DropdownModule,
+    MessagesModule,
   ],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class ContactFormComponent {
   public email: string = "";
-  public message :string = "";
+  public message: string = "";
+  public messages: Message[] = [];
 
   onSave() {
-    console.log(this.email, this.message)
+    // TODO : implémenter l'appel au service
+    this.email = "";
+    this.message = "";
+    this.messages = [
+      { severity: "success", detail: "Demande de contact envoyée avec succès" },
+    ];
   }
 }
