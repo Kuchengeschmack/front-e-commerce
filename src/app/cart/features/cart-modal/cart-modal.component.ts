db094e846f17e5afbd26b3086b3230d06893f063
Dorian Réale--Caron <drealecaron@protonmail.com>
2024-10-26 18:56:02 +0200

    refactor: clean imports

--- src/app/cart/features/cart-modal/cart-modal.component.ts
+++ src/app/cart/features/cart-modal/cart-modal.component.ts
@@ -1,23 +1,14 @@
 import { CurrencyPipe } from "@angular/common";
 import {
     Component,
-    computed,
     EventEmitter,
     Input,
-    input,
-    Output,
-    ViewEncapsulation,
+    Output
 } from "@angular/core";
-import { FormsModule } from "@angular/forms";
 import { CartService } from "app/cart/data-access/cart.service";
 import { Product } from "app/products/data-access/product.model";
-import { SelectItem } from "primeng/api";
 import { ButtonModule } from "primeng/button";
 import { DialogModule } from "primeng/dialog";
-import { DropdownModule } from "primeng/dropdown";
-import { InputNumberModule } from "primeng/inputnumber";
-import { InputTextModule } from "primeng/inputtext";
-import { InputTextareaModule } from 'primeng/inputtextarea';
 import { TableModule } from "primeng/table";
 
 @Component({
